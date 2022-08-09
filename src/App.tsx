bb7d3d92d6f535eda280a8a6c08b1c5b98f47171
baymax88 <smartbay1@outlook.com>
2022-08-08 23:28:04 -0400

    test: dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import * as React from "react";
-import Tooltip from "@mui/material/Tooltip";
+import { Dialog, Tooltip } from "@mui/material";
 
 export default function App() {
   return (
     <div className="my-2">
       <div className="text-2xl text-red-400 bg-sky-200">
-        tailwind css testing
+        tailwind css classes are working here
       </div>
       <Tooltip
         arrow
         title="testing"
         classes={{
-          tooltip: "rounded-none text-sm text-red-400"
+          tooltip: "rounded-none text-sm text-red-400",
         }}
       >
         <div className="text-red-400 text-xl font-medium">testing</div>
       </Tooltip>
+      <Dialog open={true}>
+        <div className="text-red-400 text-xl font-medium p-4">
+          tailwindcss classes are not working here
+        </div>
+      </Dialog>
     </div>
   );
 }
